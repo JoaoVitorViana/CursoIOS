ec4b17d798163f79f0cf4add473a864bfe91f37c
Joao Viana <joaovitor@live.it>
2018-04-27 00:10:47 -0400

    Aula 14

--- CursoIOS.playground/Contents.swift
+++ CursoIOS.playground/Contents.swift
@@ -2,4 +2,22 @@
 
 import UIKit
 
-var str = "Hello, playground";
+//String
+var nomeUsuario : String;
+nomeUsuario = "João Viana";
+
+//Inteiro
+var numeroInteiro: Int;
+numeroInteiro = 10;
+
+numeroInteiro = numeroInteiro + 5;
+numeroInteiro = numeroInteiro * 2;
+
+numeroInteiro = 0;
+
+var numeroDecimal : Double = 9.4
+
+var numeroFloat : Float = 3.189878
+
+print("O valor é " + String(numeroInteiro));
+print("O valor decimal é: " + String(numeroDecimal));
